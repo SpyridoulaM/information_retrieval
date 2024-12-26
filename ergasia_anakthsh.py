8a73e03bc7b597301ca02a574140de4fc15bb2a9
Tsakiris212 <Kostastsakires060a@gmail.com>
2024-12-26 17:49:15 +0200

    Update ergasia_anakthsh.py

--- ergasia_anakthsh.py
+++ ergasia_anakthsh.py
@@ -2,7 +2,13 @@
 
 import pandas as pd
 data = pd.read_csv('wiki_movie_plots_deduped.csv')
-print(data);
+print(data)
+
+
+data = data.dropna(subset=['Plot'])
+
+
+data.to_csv('cleaned_movies_dataset.csv', index=False)
 
 
 

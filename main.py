679dacb9d4fdb88ab3f744514ff24ef34d09632b
Tsakiris212 <Kostastsakires060a@gmail.com>
2024-12-26 18:05:29 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,13 +1,13 @@
 
-
+#katevasma dataset
 import pandas as pd
 data = pd.read_csv('wiki_movie_plots_deduped.csv')
 print(data)
 
-
+#katharisma tou dataset apo tis eggrafes stis opoies to plot einai keno
 data = data.dropna(subset=['Plot'])
 
-
+#apothikeush toy kathariismenou dataset
 data.to_csv('cleaned_movies_dataset.csv', index=False)
 
 
